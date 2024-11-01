6b064af46c58f9c134f52a015a6b0d7f65a1ea6e
ych <h792820406@gmail.com>
2024-11-01 13:44:42 +0800

    end |> 准备重构这个项目代码

--- main.c
+++ main.c
@@ -10,16 +10,16 @@
 #include "w5500_config.h"
 #include "w5500.h"
 
-#define W5500_INT_PIN  NRF_GPIO_PIN_MAP(0,14) 
+#define W5500_INT_PIN  NRF_GPIO_PIN_MAP(0,14)			// 配置w5500的中断引脚在 nrf52833 dk 的p0.14上 
 
-volatile uint32_t w5500_int = 0;
+// w5500中断标志位。如果w5500中断触发，该标志位会被置为true，主循环一旦检测到该标志位为true，就会立刻复位这个中断标志位。并处理w5500的中断事件
+// 这个中断标志位必须为volatile，否则由于编译器的优化行为，其可能会认为此变量的值一直是false, 从而会影响主循环对w5500中断的处理
+volatile bool w5500_itr_flag = false;					
 
-volatile bool w5500_int_flag = false;
 
-void w5500_int_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
+void w5500_itr_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
-	w5500_int++;
-	w5500_int_flag = true;
+	w5500_itr_flag = true;
 }
 
 
@@ -28,25 +28,27 @@ int main(void)
 	APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
 	NRF_LOG_DEFAULT_BACKENDS_INIT();
 
-	nrf_drv_gpiote_init();
-    nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_HITOLO(true);
-    in_config.pull = NRF_GPIO_PIN_PULLUP;
-	nrf_drv_gpiote_in_init(W5500_INT_PIN, &in_config, w5500_int_handler);
-    nrf_drv_gpiote_in_event_enable(W5500_INT_PIN, true);
+	nrf_drv_gpiote_init();			
+    nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_HITOLO(true);		// 配置dk与w5500连接的中断引脚为下降沿触发
+    in_config.pull = NRF_GPIO_PIN_PULLUP;	// 配置上拉电阻，防止中断引脚悬空导致不稳定而导致误触发中断或不触发中断
+	nrf_drv_gpiote_in_init(W5500_INT_PIN, &in_config, w5500_itr_handler);		// 初始化中断引脚, 将中断引脚1. 设置为下降沿触发，2. 设置上拉电阻，3. 设置中断处理函数 4. 设置这个配置的对应的中断引脚
+    nrf_drv_gpiote_in_event_enable(W5500_INT_PIN, true);	// 使能中断引脚的中断功能，如果不使能，即使中断引脚触发了中断，也不会触发中断处理函数
 
 	spi_init();
 
-	Load_Net_Parameters();		//装载网络参数
 	W5500_Hardware_Reset();		//硬件复位W5500
+	Load_Net_Parameters();		//装载网络参数
 	W5500_Initialization();		//W5500初始化配置
+
+
 
 	while (1)
 	{
 		W5500_Socket_Set();//W5500端口初始化配置
 
-		if (w5500_int_flag)
+		if (w5500_itr_flag)
 		{
-			w5500_int_flag = false;
+			w5500_itr_flag = false;
 			W5500_Interrupt_Process();//W5500中断处理程序框架
 			
 			if((S0_Data & S_RECEIVE) == S_RECEIVE)//如果Socket0接收到数据
--- w5500_config.h
+++ w5500_config.h
@@ -5,25 +5,62 @@
 #include "w5500.h"
 
 typedef struct {
-    uint8_t gateway_ip[4];      // 网关ip
-    uint8_t subnet_mask[4];     // 子网掩码
-    uint8_t mac_addr[6];        // MAC addr
-    uint8_t local_ip[4];        // 本机 ip
-} w5500_network_config_t;
+    uint8_t gateway_ip[4];      // Gateway IP
+    uint8_t subnet_mask[4];     // Subnet mask
+    uint8_t mac_addr[6];        // MAC address
+    uint8_t local_ip[4];        // Local IP
+} w5500_network_t;
+
+typedef enum {
+    SOCKET_MODE_CLOSE = 0,      // Socket closed
+    SOCKET_MODE_TCP_SERVER,     // TCP server mode
+    SOCKET_MODE_TCP_CLIENT,     // TCP client mode
+    SOCKET_MODE_UDP,            // UDP mode
+} socket_mode_t;
+
+typedef enum {
+    SOCKSTAT_CLOSED = 0,        // Socket closed
+    SOCKSTAT_INIT,              // Socket initialized
+    SOCKSTAT_LISTEN,            // Socket listening
+    SOCKSTAT_ESTABLISHED,       // Socket established
+    SOCKSTAT_UDP,               // Socket in UDP mode
+} socket_state_t;
+
+typedef enum {
+    SOCKDATA_NONE = 0,          // No data
+    SOCKDATA_RECEIVED,          // Data received
+    SOCKDATA_SENT,              // Data sent
+} socket_data_status_t;
 
 typedef struct {
-    uint8_t port[2];        // 端口号
-    uint8_t mode;           // 工作模式
-    uint8_t state;          // socket状态
-} w5500_socket_config_t;
+    uint8_t id;                 // Socket ID (0~7)
+    socket_mode_t mode;         // Working mode
+    socket_state_t state;       // Socket state
+    socket_data_status_t status;  // Data status
+    uint16_t local_port;        // Local port
+    uint8_t remote_ip[4];       // Remote IP (for UDP)
+    uint16_t remote_port;       // Remote port (for UDP)
+    uint8_t *p_rxbuf;           // Pointer to receive buffer
+    uint8_t *p_txbuf;           // Pointer to transmit buffer
+    uint16_t rxbuf_size;        // Receive buffer size
+    uint16_t txbuf_size;        // Transmit buffer size
+} w5500_socket_t;
+
+typedef void (*w5500_reset_func_t)(void);
+typedef void (*w5500_init_func_t)(void);
+typedef void (*w5500_set_network_func_t)(void);
+typedef void (*w5500_socket_open_func_t)(uint8_t);
+typedef void (*w5500_socket_close_func_t)(uint8_t);
 
 typedef struct {
-    w5500_network_config_t network;
-    w5500_socket_config_t  sockets[8];
-    void (*reset) (void);
-    void (*init) (void);
+    w5500_network_t network;                    // Network parameters
+    w5500_socket_t  sockets[8];                 // Socket array
+    w5500_reset_func_t reset;                   // Hardware reset function
+    w5500_init_func_t init;                     // Initialization function
+    w5500_set_network_func_t set_network;       // Set network parameters
+    w5500_socket_open_func_t socket_open;       // Open socket
+    w5500_socket_close_func_t socket_close;     // Close socket
+    // To be supplemented, other operation functions
 } w5500_dev_t;
-
-
 
 #endif
